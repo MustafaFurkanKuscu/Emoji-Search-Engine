6030f7a68a21da9872f033251bb9e79207cccb49
MustafaFurkanKuscu <mustafa.furkan.kuscu@gmail.com>
2023-02-16 21:51:40 +0300

    proje yüklendi

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+export const App = () =>{
+  return <Emojis />;
+}
+
+const Emojis =()=>{
+  const emojis=GetData("http://localhost:3004/names");
+  const [text, setText] = useState("");
+
+  const handleChangeText=(e)=>{
+    const text=e.target.value;
+    setText(text);
+    
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>🐨 Emoji Arama Motoru 🐼</h1>
+    <input className='search-input' type="text" value={text} onChange={(e)=>handleChangeText(e)}/>
+    <ul>
+      {emojis
+      .filter((emo) => emo.title.includes(text))
+      .map((emoji)=>(
+        
+        <li onMouseLeave={(e)=>handleLeave(emoji.id)} onMouseOver={(e)=>handleHover(emoji.id)} onClick={(e)=>handleClick(emoji.symbol,emoji.id)} key={emoji.id}>
+          <span>{emoji.symbol+ " "+emoji.title} </span> <span id={emoji.id} className="emoji-status"></span> 
+        </li>
+        
+      ))}
+    </ul>
+    </>
   );
 }
+
+
+const handleClick =(symbol,id)=>{
+  navigator.clipboard.writeText(symbol);
+  document.getElementById(id).innerText="Kopyalandı";
+
+}
+const handleHover =(id)=>{
+  document.getElementById(id).innerText="Emojiyi Kopyala";
+}
+const handleLeave =(id)=>{
+  document.getElementById(id).innerText="";
+}
+
+const GetData = (url) => {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch(url)
+    .then((res)=>res.json())
+    .then((json)=>setData(json))
+  }, [url]);
+  return data;
+};
+
+
 
 export default App;
